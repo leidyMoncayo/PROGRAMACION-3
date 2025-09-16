3cac933952e0be34fd900cf207318f7c34d755a5
leidyMoncayo <ljmoncayo@estudiante.uniajc.edu.co>
2025-09-16 16:18:49 +0000

    d

--- clase7/Pila.java
+++ clase7/Pila.java
@@ -6,18 +6,42 @@ public class Pila {
     public static void main(String[] args) {
 
         //creacioin de la pila numeros enteros
-        Stack<Integer> Pila = new Stack<>();
+        Stack<Integer> pila = new Stack<>();
+
+        //verificar si la pila esta vacia
+        System.out.println(pila.empty());//true
 
         //insertar elementos en la pila
-        Pila.push(8);
-        Pila.push(4);
-        Pila.push(9);
-        Pila.push(10);
-        Pila.push(6);
+        pila.push(8);
+        pila.push(4);
+        pila.push(9);
+        pila.push(10);
+        pila.push(6);
+
+       
 
         //imprimir la pila
-        System.out.println(Pila);
+        System.out.println(pila);
 
+        //Mostrar elelemnento que esta en el toppe de la pila sin removerlo
+        System.out.println("Tope de la pila: " + pila.peek());
+
+        //remover el elemneto que esta en el tope de la pila
+        System.out.println("Elemento removido del tope de la pila: " + pila.pop());
+
+        //imprimir la pila
+        System.out.println(pila);
+
+        //buscar y mostrar la posicion de un elemento dentro de la pila
+        System.out.println(pila.search(10));
+        System.out.println(pila.search(9));
+        System.out.println(pila.search(4));
+        System.out.println(pila.search(8));
+        System.out.println(pila.search(50));
+        //si al imprimir hay un -1 es que el elemento no se encuentra en la pila
+
+        //verificar si la pila esta vacia
+        System.out.println(pila.empty());//False
         
     }
 }
