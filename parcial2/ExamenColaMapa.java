627b6bb24af14074674e0d255bf39d5e0983ae84
leidyMoncayo <ljmoncayo@estudiante.uniajc.edu.co>
2025-10-14 17:22:40 +0000

    f

--- parcial2/ExamenColaMapa.java
+++ parcial2/ExamenColaMapa.java
@@ -13,9 +13,9 @@ public class ExamenColaMapa {
         private int tam;
 
         public ColaCircularEnteros(int capacidad) {
-            datos = new int[capacidad];//arreglo para almacenar los elementos de la cola
-            cabeza = 0;//posicion del primer elemento de la cola 
-            cola = 0;// posicion donde se inserta el sig elemento 
+            datos = new int[capacidad];// arreglo para almacenar los elementos de la cola
+            cabeza = 0;// posicion del primer elemento de la cola
+            cola = 0;// posicion donde se inserta el sig elemento
             tam = 0;// num de elementos en la cola
         }
 
@@ -29,18 +29,18 @@ public class ExamenColaMapa {
 
         public void encolar(int x) {
             if (estaLlena())
-                throw new IllegalStateException(" Cola llena");//verifica si la cola esta llena
+                throw new IllegalStateException(" Cola llena");// verifica si la cola esta llena
             datos[cola] = x;
-            cola = (cola + 1) % datos.length;//actualiza la posicion de la cola
-            tam++;//incrementa el tamaño de la cola
+            cola = (cola + 1) % datos.length;// actualiza la posicion de la cola
+            tam++;// incrementa el tamaño de la cola
         }
 
         public int desencolar() {
             if (estaVacia())
                 throw new IllegalStateException(" Cola vacia ");
             int v = datos[cabeza];
-            cabeza = (cabeza + 1) % datos.length;//actualiza la posicion de la cabeza
-            tam--;//decrementa el tamaño de la cola
+            cabeza = (cabeza + 1) % datos.length;// actualiza la posicion de la cabeza
+            tam--;// decrementa el tamaño de la cola
             return v;
         }
     }
@@ -48,35 +48,33 @@ public class ExamenColaMapa {
     // (50 pts) Calcula el balance de p a r n t e s i s encolando +1 y -1.
     public static int balanceConCola(String s) {
         // TODO : usar ColaCircularEnteros para encolar y luego sumar
-        ColaCircularEnteros cola = new ColaCircularEnteros(s.length());//itera sobre cada caracter de la cadena s
+        ColaCircularEnteros cola = new ColaCircularEnteros(s.length());// itera sobre cada caracter de la cadena s
 
         for (int i = 0; i < s.length(); i++) {
             char parentesis = s.charAt(i);
             if (parentesis == '(') {
-                cola.encolar(1);//si es un parentesis de apertura encola 1
-            } 
-                else if (parentesis== ')') {
-                cola.encolar(-1);//si es un parentesis de cierre encola -1
+                cola.encolar(1);// si es un parentesis de apertura encola 1
+            } else if (parentesis == ')') {
+                cola.encolar(-1);// si es un parentesis de cierre encola -1
             }
         }
-        return 0;//retorna 0 si la cola esta vacia o si desencola
+        return 0;// retorna 0 si la cola esta vacia o si desencola
     }
 
     // (50 pts) Registra intentos por nombre en un mapa .
-    public static int registrarIntento(Map<String, Integer> intentos,String nombre) {
+    public static int registrarIntento(Map<String, Integer> intentos, String nombre) {
         // TODO : incrementar si existe o iniciar en 1 si no existe
-        if (intentos.containsKey (nombre)) {//verifica
-            int cont= intentos.get(nombre);
+        if (intentos.containsKey(nombre)) {// verifica
+            int cont = intentos.get(nombre);
             intentos.put(nombre, cont + 1);
             return cont + 1;
-        }
-        else {//si no esxiste el nombre en el mapa inicia en 1
+        } else {// si no esxiste el nombre en el mapa inicia en 1
             intentos.put(nombre, 1);
-            return 1;//retorna 1 si no existe el nombre en el mapa
+            return 1;// retorna 1 si no existe el nombre en el mapa
         }
 
-}
-            
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String cadena = sc.nextLine();
